fc4833ef96bbde9a30c47c04c428c1fe803ab849
JorgePlazaC <chocheplaza@hotmail.com>
2023-01-10 17:41:14 -0300

    Eliminar y casi editar

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { useState, useEffect, useContext } from "react";
 import { initializeApp } from "firebase/app";
-import { getDatabase, onValue, push, ref, set } from "firebase/database";
-import { Table, Button, Modal} from "react-bootstrap";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  set,
+  remove,
+} from "firebase/database";
+import { Table, Button, Modal, Col, Form, InputGroup, Row } from "react-bootstrap";
+
 
 //Propio del proyecto
 import JuzgadoContext from "../components/JuzgadoContext";
@@ -10,7 +18,7 @@ import { DespliegueDatos } from "../components/DespliegueDatos";
 
 export const Home = () => {
   const [array, setArray] = useState([]);
-  const [arrayTodo, setArrayTodo] = useState([])
+  const [arrayTodo, setArrayTodo] = useState([]);
   const [cargando, setCargando] = useState(true);
   const [paginasMax, setPaginasMax] = useState(10);
   const [pagActual, setPagActual] = useState(1);
@@ -19,7 +27,12 @@ export const Home = () => {
   const [anteriorDisponible, setAnteriorDisponible] = useState(false);
   const [inputBuscar, setInputBuscar] = useState();
   const [modal, setModal] = useState(false);
-  const [observacion,setObservacion] = useState()
+  const [modalEdicion, setModalEdicion] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [observacion, setObservacion] = useState();
+  const [datoCopia, setDatoCopia] = useState("");
 
   let arraySeleccion = [];
 
@@ -62,11 +75,11 @@ export const Home = () => {
     onValue(reference, (snapshot) => {
       const data = [];
       let cont = 0;
-      if(arraySeleccion.length >= 1){
-        arraySeleccion=[]
+      if (arraySeleccion.length >= 1) {
+        arraySeleccion = [];
       }
       snapshot.forEach((dato) => {
-        arraySeleccion.push(dato.val())
+        arraySeleccion.push(dato.val());
         if (
           (pagActual - 1) * paginasMax <= cont &&
           pagActual * paginasMax > cont
@@ -78,8 +91,8 @@ export const Home = () => {
         //console.log(cont)
         cont++;
       });
-      console.log(arraySeleccion)
-      setArrayTodo(arraySeleccion)
+      console.log(arraySeleccion);
+      setArrayTodo(arraySeleccion);
       setArray(data);
       //Paginacion()
       setCargando(false);
@@ -92,7 +105,7 @@ export const Home = () => {
     //console.log(suma);
     setPagActual(suma);
     fetchFirebase();
-    
+
     //Paginacion();
     //console.log(suma);
     if (arraySeleccion.length < 9) {
@@ -105,7 +118,6 @@ export const Home = () => {
     } else {
       setAnteriorDisponible(true);
     }
-    
   };
 
   //Siguiente página
@@ -116,10 +128,10 @@ export const Home = () => {
     fetchFirebase();
     //Paginacion();
     //console.log(suma);
-    let totalPaginas = arraySeleccion.length/10
-    console.log("Suma: "+suma)
-    console.log("El total de paginas es: "+totalPaginas)
-    
+    let totalPaginas = arraySeleccion.length / 10;
+    console.log("Suma: " + suma);
+    console.log("El total de paginas es: " + totalPaginas);
+
     if (suma > totalPaginas) {
       setSiguienteDisponible(false);
     } else {
@@ -132,19 +144,35 @@ export const Home = () => {
     }
   };
 
-  const Busqueda = (event) =>{
-    let arrayBusqueda = []
+  const Busqueda = (event) => {
+    let arrayBusqueda = [];
     //console.log(arrayTodo)
-    arrayTodo.forEach((dato,i) =>{
-      if(dato.nombre === inputBuscar){
-        arrayBusqueda.push(dato)
+    arrayTodo.forEach((dato, i) => {
+      if (dato.nombre === inputBuscar) {
+        arrayBusqueda.push(dato);
       }
-    })
-    setArray(arrayBusqueda)
-    console.log(array)
+    });
+    setArray(arrayBusqueda);
+    console.log(array);
     //setPagActual(1)
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
+
+  const BorrarDato = (dato) => {
+    const db = getDatabase();
+    let reference = ref(db, "datos/");
+    console.log("Borrar");
+    reference = ref(db, "datos/" + dato.key);
+    if(window.confirm("Estás seguro que deseas eliminar "+dato.nombre)){
+      remove(reference, dato)
+      .then(alert("Eliminación exitosa de: " + dato.nombre))
+      .catch((error) => {
+        alert(error);
+      });
+      return
+    }
+    alert("Eliminación cancelada")
+  };
 
   /*
   //Paginación
@@ -192,6 +220,7 @@ export const Home = () => {
             <th>Correo</th>
             <th>Telefono</th>
             <th>Observaciones</th>
+            <th>Acciones</th>
           </tr>
         </thead>
         {array.map((dato, i) => (
@@ -200,10 +229,40 @@ export const Home = () => {
               <td>{dato.nombre}</td>
               <td>{dato.correo}</td>
               <td>{dato.telefono}</td>
-              <td><Button variant="primary" onClick={() =>{handleShow()
-              setObservacion(dato.observaciones)}}>
-        Abrir
-      </Button></td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    handleShow();
+                    setObservacion(dato.observaciones);
+                  }}
+                >
+                  Abrir
+                </Button>
+              </td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    handleShowEdicion();
+                    setNombre(dato.nombre);
+                    setCorreo(dato.correo)
+                    setTelefono(dato.telefono)
+                    setObservacion(dato.observaciones)
+                    console.log(dato.observaciones)
+                  }}
+                >
+                  Editar
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    BorrarDato(dato);
+                  }}
+                >
+                  Borrar
+                </Button>
+              </td>
             </tr>
           </tbody>
         ))}
@@ -228,7 +287,6 @@ export const Home = () => {
       )}
     </div>
   );
-
 
   /*
   const SeleccionArray = () => {
@@ -270,26 +328,49 @@ export const Home = () => {
 
   const handleClose = () => setModal(false);
   const handleShow = (observacion) => {
-    console.log(observacion)
-    setObservacion(observacion)
-    setModal(true)
+    console.log(observacion);
+    setObservacion(observacion);
+    setModal(true);
+  };
+  const handleCloseEdicion = () => setModalEdicion(false);
+  const handleShowEdicion = () => {
+    setModalEdicion(true);
   };
 
+  const editarNombre = (event) => {
+    setNombre(event.target.value);
+    console.log(nombre);
+  };
+
+  const editarCorreo = (event) => {
+    setCorreo(event.target.value);
+    console.log(correo);
+  };
+
+  const editarTelefono = (event) => {
+    setTelefono(event.target.value);
+    console.log(telefono);
+  };
+
+  const editarObservaciones = (event) => {
+    setObservacion(event.target.value);
+    console.log(observacion);
+  };
   return (
     <div className="mt-3 container">
       <form class="form-inline my-2 my-lg-0" onSubmit={Busqueda}>
-            <input
-              class="form-control mr-sm-2"
-              type="text"
-              aria-label="Search"
-              required
-              value={inputBuscar}
-              onChange={editarInputBusqueda}
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Buscar
-            </button>
-          </form>
+        <input
+          class="form-control mr-sm-2"
+          type="text"
+          aria-label="Search"
+          required
+          value={inputBuscar}
+          onChange={editarInputBusqueda}
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Buscar
+        </button>
+      </form>
       <div class="container">{DesplegarDatos()}</div>
       <Modal show={modal} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -298,6 +379,59 @@ export const Home = () => {
         <Modal.Body>{observacion}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={modalEdicion} onHide={handleCloseEdicion}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edición de datos:</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control required type="text" value={nombre} onChange={editarNombre} />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom02">
+                <Form.Label>Correo</Form.Label>
+                <Form.Control required type="text" value={correo} onChange={editarCorreo} />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom03">
+                <Form.Label>Telefono</Form.Label>
+                <Form.Control required type="text" value={telefono} onChange={editarTelefono} />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <div className="mx-auto">
+              <Row className="mx-auto">
+                <Form.Group
+                  className="mx-auto"
+                  as={Col}
+                  md="4"
+                  controlId="validationCustom04"
+                >
+                  <Form.Label>Observaciones</Form.Label>
+                  <Form.Control
+                    size="lg"
+                    required
+                    type="text"
+                    value={observacion}
+                    onChange={editarObservaciones}
+                    rows={5}
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+            </div>
+            <Button type="submit">Ingresar</Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseEdicion}>
             Cerrar
           </Button>
         </Modal.Footer>
